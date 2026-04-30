c115f8d83eb6574e50936e589dbe4685a3020217
yujinnie <yujinnieohh@gmail.com>
2026-04-30 23:40:47 +0900

    feature: 레이아웃 UI 작업 진행 중(아이콘 렌더링 이슈 수정 예정)

--- src/components/common/TopNav.tsx
+++ src/components/common/TopNav.tsx
@@ -5,7 +5,7 @@ export default function TopNav() {
     <nav className="fixed top-0 z-50 w-full bg-transparent">
       <div className="flex items-center justify-between px-7 py-3">
         <div className="flex items-center gap-6">
-          <Image src="/assets/icons/ic-netfilx.svg" alt="Netflix" width={56} height={57} />
+          <Image src="/assets/icons/ic-netfilx.svg" alt="Netflix" width={56} height={56} />
           <ul className="text-body1 flex gap-4 text-white">
             <li className="cursor-pointer">TV Shows</li>
             <li className="cursor-pointer">Movies</li>
