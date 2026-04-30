3bd3c76b3d92e298e0fa849f60b4a496ea029788
yujinnie <yujinnieohh@gmail.com>
2026-05-01 01:55:56 +0900

    feature: Featured 컴포넌트 수정

--- src/components/main/Featured.tsx
+++ src/components/main/Featured.tsx
@@ -19,28 +19,29 @@ export default function Featured() {
   if (!movie) return <div className="h-[60vh] bg-black" />;
 
   return (
-    <section className="relative h-[80vh] w-full">
+    <section className="relative w-full">
       {/* 배경 포스터 */}
-      <Image
-        src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
-        alt={movie.title}
-        fill
-        className="object-cover"
-        priority
-      />
-
+      <div className="relative h-[55vh] w-full">
+        <Image
+          src={`https://image.tmdb.org/t/p/original${movie.poster_path}`}
+          alt={movie.title}
+          fill
+          className="object-cover"
+          priority
+        />
+        <div className="absolute inset-0 bg-linear-to-t from-black via-transparent to-transparent" />
+      </div>
+      {/* Top 10 배지 영역 (임의로 #1 설정) */}
+      <div className="mb-5 flex items-center justify-center gap-1.5">
+        <Image src="/assets/icons/ic-top10.svg" alt="TOP 10" width={24} height={24} />
+        <span className="text-body2 font-bold tracking-tight text-white">#1 in Korea Today</span>
+      </div>
       <div className="bg-main-gradient absolute inset-0" />
 
-      {/* 하단 버튼 및 정보 영역 */}
-      <div className="absolute bottom-10 left-0 flex w-full flex-col items-center">
-        {/* Top 10 배지 영역 (임의로 #2 설정) */}
-        <div className="mb-5 flex items-center gap-1.5">
-          <Image src="/assets/icons/ic-top10.svg" alt="TOP 10" width={24} height={24} />
-          <span className="text-body2 font-bold tracking-tight text-white">#1 in Korea Today</span>
-        </div>
-
+      {/* 하단 버튼 영역 */}
+      <div className="flex flex-col items-center py-6">
         {/* 버튼 그룹 */}
-        <div className="flex w-full items-center justify-center gap-10 px-10">
+        <div className="flex w-full items-center justify-center gap-10 px-3">
           <button className="flex flex-col items-center gap-1 text-white">
             <Image src="/assets/icons/ic-add.svg" alt="My List" width={24} height={24} />
             <span className="text-caption1">My List</span>
