41738952e73ce816d6f90ab7b12e467976a7f5cf
yeobi <kimhyjjang@gmail.com>
2026-05-01 12:05:34 +0900

    feature: 랜딩 페이지 구현

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,11 +1,14 @@
 import Featured from '@/components/main/Featured';
 import Previews from '@/components/main/Previews';
+import SplashScreen from '@/components/landing/SplashScreen';
 
 export default function Home() {
   return (
-    <div className="flex flex-col">
-      <Featured />
-      <Previews />
-    </div>
+    <SplashScreen>
+      <div className="flex flex-col">
+        <Featured />
+        <Previews />
+      </div>
+    </SplashScreen>
   );
 }
